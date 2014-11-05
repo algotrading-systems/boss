45c7e5e82f98c469a5f01ef1be9610a9ecf4e765
Alexander Emelyanov <alexander.emelyanov@faunusanalytics.com>
2014-11-05 14:23:00 +0300

    Mobile application switched to BOSS Mobile Service

--- BinaryOptionsSignals/BinaryOptionsSignals.WindowsPhone/services/mobileServices/settings/bos.js
+++
@@ -1,4 +0,0 @@
-﻿// http://go.microsoft.com/fwlink/?LinkID=290993&clcid=0x419
-var bosClient = new WindowsAzure.MobileServiceClient(
-                "https://bos.azure-mobile.net/",
-                "IZmOEUIsVKAVTaoUkZTWUjlIIvQSeJ77");
