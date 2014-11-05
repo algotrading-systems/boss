9993b5f029b8a281346e3ae3d499af36bce0d5ca
Alexander Emelyanov <alexander.emelyanov@faunusanalytics.com>
2014-11-05 21:16:19 +0300

    Full refactoring... Data.loadFromCloud() method is promise. Usable!

--- BinaryOptionsSignals/BinaryOptionsSignals.Shared/js/data.js
+++ BinaryOptionsSignals/BinaryOptionsSignals.Shared/js/data.js
@@ -8,27 +8,29 @@
         14400: new WinJS.Binding.List(),
     };
 
+    function loadFromCloud() {
+        return new WinJS.Promise(function (completeDispatch, errorDispatch, progressDispatch) {
+            function loadIndicators() {
+                bossClient.getTable("Indicator").read().done(function (indicators) {
+                    indicators.forEach(function (indicator) {
+                        if (indicator.time_frame in indicatorsLists) {
+                            indicatorsLists[indicator.time_frame].push(indicator);
+                        }
+                    });
+                    completeDispatch(indicators.length);
+                });
+            };
+            setImmediate(loadIndicators, {});
+        });
+    }
+
+
     WinJS.Namespace.define("Data", {
         indicators900: indicatorsLists[900],
         indicators1800: indicatorsLists[1800],
         indicators3600: indicatorsLists[3600],
         indicators14400: indicatorsLists[14400],
+        loadFromCloud: loadFromCloud
     });
-
-
-    function loadIndicatorsFromCloud() {
-        bossClient.getTable("Indicator").read().done(function (indicators) {
-            indicators.forEach(function (indicator) {
-                if (indicator.time_frame in indicatorsLists) {
-                    indicatorsLists[indicator.time_frame].push(indicator);
-                }
-            });
-        }, function (error) {
-            /** @TODO Add error handling */
-            var x = error;
-        });
-    };
-
-    loadIndicatorsFromCloud();
     
 })();
