4e233e444602505b3eb2aa6c6d7f7480b508e65c
Alexander Emelyanov <alexander.emelyanov@faunusanalytics.com>
2014-11-06 21:03:27 +0300

    #dev Error handler use dialog window for reconnect to Azure Cloud

--- BinaryOptionsSignals/BinaryOptionsSignals.Shared/js/data.js
+++ BinaryOptionsSignals/BinaryOptionsSignals.Shared/js/data.js
@@ -19,6 +19,18 @@
                     });
                     completeDispatch(indicators.length);
                 }, function (error) {
+                    // Create the message dialog and set its content
+                    var msg = new Windows.UI.Popups.MessageDialog("No internet connection has been found.");
+                    // Add commands and set their command handlers
+                    msg.commands.append(new Windows.UI.Popups.UICommand("Try again", Data.loadFromCloud));
+                    msg.commands.append(new Windows.UI.Popups.UICommand("Ignore", function () {}));
+                    // Set the command that will be invoked by default
+                    msg.defaultCommandIndex = 0;
+                    // Set the command to be invoked when escape is pressed
+                    msg.cancelCommandIndex = 1;
+                    // Show the message dialog
+                    msg.showAsync();
+                    // Temporary ugly error handling
                     completeDispatch(0);
                     // errorDispatch(error);
                 });
